c9a3f352f1e6cfd8129dd03e6cf0030c6a4a915e
saurabhparasar <saurabhparasar3010@gmail.com>
2022-01-03 23:41:58 +0530

    api integrated

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,13 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Styles/Signin.css";
+import axios from "axios";
 export default function Signin() {
+  const [username, setusername] = useState("");
+  const [password, setpassword] = useState("");
+  const [token, settoken] = useState("");
+
+  function onSubmit(e) {
+    let data = localStorage.getItem("userdetails");
+    data = JSON.parse(data);
+    let formdata = { username: "", password: "" };
+    formdata.username = username;
+    formdata.password = password;
+    let input = JSON.stringify(formdata);
+    let requestOptions = {
+      method: "POST",
+      body: input,
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    };
+    axios
+      .post("https://plant1tree.herokuapp.com/signin", requestOptions)
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        if (typeof json.token !== "undefined") {
+          settoken(json.token);
+
+          let getreq = {
+            method: "GET",
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+              Authorization: "Token " + token,
+            },
+          };
+          axios
+            .get("https://plant1tree.herokuapp.com/viewUsers", getreq)
+            .then((response) => {
+              console.log(response);
+            });
+        }
+      });
+  }
+  function User_name(e) {
+    const data = e.target.value;
+    setusername(data);
+  }
+  function UserPassword(e) {
+    const data = e.target.value;
+    setpassword(data);
+  }
   return (
     <div className="login-page">
       <div className="form">
         <form className="register-form">
-          <input type="text" placeholder="name" />
-          <input type="password" placeholder="password" />
-          <input type="text" placeholder="email address" />
+          <input
+            type="password"
+            placeholder="password"
+            onChange={UserPassword}
+            value={setpassword}
+          />
+          <input
+            type="text"
+            placeholder="email address"
+            onChange={User_name}
+            value={setusername}
+          />
           <button>create</button>
           <p className="message">
             Already registered? <a href="#">Sign In</a>
@@ -16,7 +77,7 @@ export default function Signin() {
         <form className="login-form">
           <input type="text" placeholder="username" />
           <input type="password" placeholder="password" />
-          <button>login</button>
+          <button onClick={() => onSubmit()}>login</button>
           <p className="message">
             Not registered? <a href="signup">Create an account</a>
           </p>
